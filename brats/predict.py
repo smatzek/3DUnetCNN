bed5fd2281b59f5035658301be56d656eb529f18
Samuel Matzek <smatzek@us.ibm.com>
2019-12-10 08:51:58 -0600

    Add command line parameters to predict.py for easier scripting
    Add command line parameters to the predict.py file to
    enable easier scripting of the prediction call for multiple
    models.

--- brats/predict.py
+++ brats/predict.py
@@ -1,11 +1,31 @@
 import os
+import argparse
 
 from train import config
 from unet3d.prediction import run_validation_cases
 
 
 def main():
-    prediction_dir = os.path.abspath("prediction")
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data_file_path', type=str,
+                      default='brats_data.h5',
+                      help='Path to the h5 data file containing training and '
+                           'validation subjects.')
+    parser.add_argument('--model_file', type=str,
+                      default='isensee_2017_model.h5',
+                      help='Path to the aved model file.')
+    parser.add_argument('--output_dir', type=str,
+                      default='prediction',
+                      help='Output directory')
+    parser.add_argument('--validation_ids_file', type=str,
+                      default='isensee_validation_ids.pkl',
+                      help='The pkl file containing the validation IDs.')
+    FLAGS = parser.parse_args()
+    config['validation_file'] = FLAGS.validation_ids_file
+    config['model_file'] = FLAGS.model_file
+    config['data_file'] = FLAGS.data_file_path
+
+    prediction_dir = os.path.abspath(FLAGS.output_dir)
     run_validation_cases(validation_keys_file=config["validation_file"],
                          model_file=config["model_file"],
                          training_modalities=config["training_modalities"],
